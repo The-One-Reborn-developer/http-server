4635b73f41cf6d62925b9c4bf99292ffe5ac1443
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-29 08:34:03 +0300

    feat: 200 response

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # HTTP server
 
 **Basic server for basic needs.**
+
+## Types of requests
+
+- GET
+
+## Types of responses
+
+- 200 OK
--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # Wait for client
+    server_socket.listen(1)
+
+    while True:
+        client_socket, _ = server_socket.accept()  # Wait for client
+        try:
+            client_socket.recv(1024)  # Receive HTTP request
+            client_socket.sendall(b"HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!")  # Send HTTP response
+        except BrokenPipeError:
+            print("BrokenPipeError: client disconnected")
+        finally:
+            client_socket.close()
 
 
 if __name__ == "__main__":
