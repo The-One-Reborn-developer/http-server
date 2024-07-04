9cb469ae3a619bc717e90afbe60a28e4e505bbe3
Adil Arshidinov <mortonmarrion@gmail.com>
2024-07-04 05:43:58 +0300

    feat: more comments in handle_client function

--- main.py
+++ main.py
@@ -15,19 +15,25 @@ def handle_client(client_socket, client_address):
     method = parsed_request[0].split(" ")[0]
     url = request.split(" ")[1]
 
+    # Handle GET requests
     if method == "GET":
+        # If the URL is "/", return a 200 OK response
         if url == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n"
+        # If the URL starts with "/echo/", return the provided string
         elif url.startswith('/echo/'):
             string = url.split("/echo/")[1]
+            # If the client supports gzip compression, compress the string
             if 'Accept-Encoding' in request:
                 compressed_string = gzip.compress(string.encode('utf-8'))
                 response = (
                     f'HTTP/1.1 200 OK\r\n'
                     'Content-Type: text/plain\r\n'
-                    f'Content-Length: {len(string)}\r\n\r\n'
+                    f'Content-Length: {len(string)}\r\n'
+                    'Content-Encoding: gzip\r\n\r\n'
                     f'{compressed_string}'
                 ).encode('utf-8')
+            # If the client does not support gzip compression, return the string as is
             else:
                 response = (
                     f'HTTP/1.1 200 OK\r\n'
@@ -35,6 +41,7 @@ def handle_client(client_socket, client_address):
                     f'Content-Length: {len(string)}\r\n\r\n'
                     f'{string}'
                 ).encode('utf-8')
+        # If the URL starts with "/user-agent", return the user agent string
         elif url.startswith('/user-agent'):
             user_agent = parsed_request[3].split(': ')[1]
             response = (
@@ -43,6 +50,7 @@ def handle_client(client_socket, client_address):
                 f'Content-Length: {len(user_agent)}\r\n\r\n'
                 f'{user_agent}'
             ).encode('utf-8')
+        # If the URL starts with "/files", return the content of the specified file
         elif url.startswith('/files'):
             file_path = url.split("/files/")[1]
             with open(file_path, 'rb') as file:
@@ -53,13 +61,17 @@ def handle_client(client_socket, client_address):
                 f'Content-Length: {len(file_data)}\r\n\r\n'
                 f'{file_data.decode("utf-8")}'
             ).encode('utf-8')
+        # If the URL does not match any of the above, return a 404 Not found response
         else:
             response = "HTTP/1.1 404 Not found\r\n\r\n".encode('utf-8')
+    # Handle POST requests
     elif method == "POST":
         file_path = url.split("/files/")[1]
+        # Write the request body to the specified file
         with open(file_path, 'wb') as file:
             file.write(parsed_request[-1].encode())
         response = "HTTP/1.1 201 Created\r\n\r\n".encode('utf-8')
+    # If the method is neither GET nor POST, return a 400 Bad Request response
     else:
         response = "HTTP/1.1 400 Bad Request\r\n\r\n".encode('utf-8')
 
